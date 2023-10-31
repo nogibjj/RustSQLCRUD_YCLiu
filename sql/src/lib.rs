70c10a474786e414cb1c668a54843c6ebe046490
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2023-10-31 15:10:40 +0000

    update CRUD

---
+++ sql/src/lib.rs
@@ -0,0 +1,65 @@
+// lib.rs
+use rusqlite::{params, Connection, Result};
+
+#[derive(Debug)]
+struct Customer {
+    cust_id: String,
+    name: String,
+    sex: String,
+}
+
+pub fn create(conn: &Connection) -> Result<()> {
+    
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS Customer (
+            cust_id TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL,
+            sex TEXT NOT NULL
+        )",
+        [],
+    )?;
+    
+    Ok(())
+}
+
+pub fn insert(conn: &Connection) -> Result<()> {
+    conn.execute( "INSERT INTO Customer (cust_id, name, sex)
+              VALUES (?1, ?2, ?3)", params!["001", "John", "Male"])?;
+    conn.execute( "INSERT INTO Customer (cust_id, name, sex)
+              VALUES (?1, ?2, ?3)", params!["002", "Devin", "Female"])?;              
+    conn.execute( "INSERT INTO Customer (cust_id, name, sex)
+              VALUES (?1, ?2, ?3)", params!["003", "Sharon", "Female"])?;                  
+    Ok(())                        
+            
+}
+
+pub fn query(conn: &Connection) -> Result<()> {
+    let mut stmt = conn.prepare("SELECT * FROM Customer")?;
+    let rows = stmt.query_map([], |row| {
+        Ok((
+            row.get::<usize, String>(0)?,
+            row.get::<usize, String>(1)?,
+            row.get::<usize, String>(2)?
+        ))
+    })?;
+    for row in rows {
+        println!("{:?}", row?);
+    }
+    Ok(())
+}
+
+pub fn update(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "UPDATE Customer SET sex = 'Unknown' WHERE cust_id = '001'",
+        [],
+    )?;
+    Ok(())
+}
+
+pub fn delete(conn: &Connection) -> Result<()> {
+    conn.execute(
+        "DELETE FROM Customer WHERE cust_id = '001' ",
+        [],
+    )?;
+    Ok(())
+}
--- sql/src/main.rs
+++ sql/src/main.rs
@@ -1,32 +1,36 @@
-
+use std::env;
 use rusqlite::{Connection, Result};
 
-fn main ()-> Result<()> {
-    println!("Create Database...");
-    let conn = Connection::open("Transactions.db")?;
-    
-    println!("Create Customer Table...");
-    conn.execute(
-        "create table if not exists Customer (
-            cust_id TEXT, name TEXT, sex TEXT);
-            INSERT INTO Customer VALUES ('001','John','Male');
-            INSERT INTO Customer VALUES ('002','Devin','Female');
-            INSERT INTO Customer VALUES ('003','Sharon','Female');",()
-    )?;
-    println!("Querying...");
-    //Read (Select)
-    //let mut stmt = conn.prepare("SELECT * FROM Customer;")?;
-    
-    //let rows = stmt.query_map([], |row| {
-    //    Ok((row.get(0)?, row.get(1)?, row.get(2)?))
-    //})?;
-    
 
-    //for row in rows {
-    //    let (cust_id, name, sex): (String, String, String) = row?;
-    //    println!("Customer ID: {}, Name: {}, Sex: {}", cust_id, name, sex);
-    //}
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("Error: {:?}", err);
+    }
+}
+
+fn run() -> Result<(), rusqlite::Error> {
+    let conn = Connection::open("Transactions.db")?;
+
+    println!("Creating table...");
+    DBFunc::create(&conn)?;
+        
+    println!("Inserting data...");
+    DBFunc::insert(&conn)?;
+    
+    println!("Querying data...");
+    DBFunc::query(&conn)?;
+
+    println!("Updating data...");
+    DBFunc::update(&conn)?;
+
+    println!("Querying data...");
+    DBFunc::query(&conn)?;
+
+    println!("Deleting data...");
+    DBFunc::delete(&conn)?;
+
+    println!("Querying data...");
+    DBFunc::query(&conn)?;    
 
     Ok(())
-}    
-
+}
